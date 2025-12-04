01bbc6aab8431e72d3d76f1f8eaffea2bf0f8a43
sanjayk63806-sank <sanjayk63806@gmail.com>
2025-12-04 13:03:39 +0530

    Add files via upload
    day1

---
+++ learnautomation/automation.java
@@ -0,0 +1,11 @@
+package learnautomation;
+
+public class automation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
